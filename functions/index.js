3a26f2ec2cd5fa0c576b1dc68bdca836c90b0529
mrron313 <ez.boy763@gmail.com>
2022-06-22 18:40:49 +0600

    convert rest api to trigger-- when new user signs up, he will be subscribed to mailerlite group

--- functions/index.js
+++ functions/index.js
@@ -5,37 +5,35 @@ const cors = require('cors')({origin: true});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.subscribeUser = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    console.log(request);
-    // MailerLite API
-    var data = JSON.stringify({
-      "email": request.body.email,
-      "resubscribe": false,
-      "autoresponders": true,
-      "type": null
-    });
-  
-    var config = {
-      method: 'post',
-      url: 'https://api.mailerlite.com/api/v2/groups/111324133/subscribers',
-      headers: { 
-        'X-MailerLite-ApiDocs': 'true', 
-        'X-MailerLite-ApiKey': 'API_KEY', 
-        'Content-Type': 'application/json'
-      },
-      data : data
-    };
-  
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      response.send(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-      response.send(error);
-    });
+exports.subscribeUser = functions.auth.user().onCreate((user) => {
+  console.log('new user signed up', user);
+  // MailerLite API
+  var data = JSON.stringify({
+    "email": user.email,
+    "resubscribe": false,
+    "autoresponders": true,
+    "type": null
+  });
+
+  var config = {
+    method: 'post',
+    url: 'https://api.mailerlite.com/api/v2/groups/111324133/subscribers',
+    headers: { 
+      'X-MailerLite-ApiDocs': 'true', 
+      'X-MailerLite-ApiKey': 'API_KEY', 
+      'Content-Type': 'application/json'
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    response.send(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+    response.send(error);
   });
 });
 
