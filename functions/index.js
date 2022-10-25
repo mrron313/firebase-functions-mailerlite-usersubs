b1d63b886526806491376efe604108247618364c
mrron313 <ez.boy763@gmail.com>
2022-10-25 11:18:12 +0600

    latest changes

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,8 @@ const functions = require("firebase-functions");
 const axios = require('axios');
 const cors = require('cors')({origin: true});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.subscribeUser = functions.auth.user().onCreate((user) => {
-  console.log('new user signed up', user);
+  console.log('new user signed up from wp and app', user);
   // MailerLite API
   var data = JSON.stringify({
     "email": user.email,
@@ -20,7 +17,7 @@ exports.subscribeUser = functions.auth.user().onCreate((user) => {
     url: 'https://api.mailerlite.com/api/v2/groups/111324133/subscribers',
     headers: { 
       'X-MailerLite-ApiDocs': 'true', 
-      'X-MailerLite-ApiKey': 'API_KEY', 
+      'X-MailerLite-ApiKey': 'c10567d2d3efdaba7958420854af1f96', 
       'Content-Type': 'application/json'
     },
     data : data
@@ -29,11 +26,42 @@ exports.subscribeUser = functions.auth.user().onCreate((user) => {
   axios(config)
   .then(function (response) {
     console.log(JSON.stringify(response.data));
-    response.send(JSON.stringify(response.data));
   })
   .catch(function (error) {
     console.log(error);
-    response.send(error);
   });
 });
 
+exports.subscribeUserFromWP = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    res.send("hello"+ req.body.email);
+
+    console.log('new user signed up from wp', req.body);
+    // MailerLite API
+    var data = JSON.stringify({
+      "email": req.body.email,
+      "resubscribe": false,
+      "autoresponders": true,
+      "type": null
+    });
+  
+    var config = {
+      method: 'post',
+      url: 'https://api.mailerlite.com/api/v2/groups/111335351/subscribers',
+      headers: { 
+        'X-MailerLite-ApiDocs': 'true', 
+        'X-MailerLite-ApiKey': 'c10567d2d3efdaba7958420854af1f96', 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+  
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  });
+});
